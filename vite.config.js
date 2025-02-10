b6b819df2a9d68b8732a57e35cdd0665ed5992d8
e-boot <ebootinbox@gmail.com>
2025-02-10 18:00:18 +0000

    update title

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-site/' // Replace with your repository name
+  base: '/eboot-site/' // Updated to match your repository name
 })
